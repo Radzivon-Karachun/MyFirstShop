27a330f3bcb7dfcdad231f6b316c3f03d8a2a18c
Radzivon-Karachun <karachun1988@gmail.com>
2021-04-09 22:13:14 +0200

    add category selection in admin forms

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import *
+from django.forms import ModelChoiceField
 
 # Register your models here.
+
+
+class NotebookAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "category":
+            return ModelChoiceField(Categories.objects.filter(slug='notebooks'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class SmartphoneAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "category":
+            return ModelChoiceField(Categories.objects.filter(slug='smartphones'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Categories)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
+admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(Notebook, NotebookAdmin)
