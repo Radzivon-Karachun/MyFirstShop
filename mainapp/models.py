5dd9837868c41dd0f76c6c01b607cfd5815d2f39
Radzivon-Karachun <karachun1988@gmail.com>
2021-04-09 22:08:09 +0200

    new models: notebook, smartfones

--- mainapp/models.py
+++ mainapp/models.py
@@ -7,16 +7,8 @@ User = get_user_model()
 
 
 # Create your models here.
-# TODO:  1.Category
-# TODO:  2.Product
-# TODO:  3.CartProduct
-# TODO:  4.Cart
-# TODO:  5.Order
-# TODO:  6.Customer
-# TODO:  7.Specifications
 
-class Category(models.Model):
-
+class Categories(models.Model):
     name = models.CharField(max_length = 255, verbose_name = 'Category Name')
     slug = models.SlugField(unique = True)
 
@@ -25,11 +17,10 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-
     class Meta:
         abstract = True
 
-    category = models.ForeignKey(Category, verbose_name = 'Category', on_delete = models.CASCADE)
+    category = models.ForeignKey(Categories, verbose_name = 'Category', on_delete = models.CASCADE)
     title = models.CharField(max_length = 255, verbose_name = 'Name')
     slug = models.SlugField(unique = True)
     image = models.ImageField(verbose_name = 'Pictures')
@@ -40,8 +31,36 @@ class Product(models.Model):
         return self.title
 
 
-class CartProduct(models.Model):
+class Notebook(Product):
+    diagonal = models.CharField(max_length = 255, verbose_name = 'Diagonal')
+    display_type = models.CharField(max_length = 255, verbose_name = 'Display type')
+    processor_freq = models.CharField(max_length = 255, verbose_name = 'Frequency processor')
+    ram = models.CharField(max_length = 255, verbose_name = 'RAM')
+    video = models.CharField(max_length = 255, verbose_name = 'Video card')
+    time_without_charge = models.CharField(max_length = 255, verbose_name = 'Battery operating time')
 
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
+
+
+class Smartphone(Product):
+    display_diagonal = models.CharField(max_length = 255, verbose_name = 'Diagonal display')
+    display_type = models.CharField(max_length = 255, verbose_name = 'Display type')
+    display_resolution = models.CharField(max_length = 255, verbose_name = 'Display resolution')
+    battery_volume = models.CharField(max_length = 255, verbose_name = 'Battery volume')
+    processor_freq = models.CharField(max_length = 255, verbose_name = 'Frequency processor')
+    ram = models.CharField(max_length = 255, verbose_name = 'RAM')
+    sd_card = models.BooleanField(default = True)
+    sd_card_volume_max = models.CharField(max_length = 255, verbose_name = 'Max volume SD-Card')
+    qty_sim = models.DecimalField(max_digits = 1, decimal_places = 0, verbose_name = 'SIM Quantity')
+    rear_cam_mp = models.CharField(max_length = 255, verbose_name = 'Rear camera resolution')
+    front_cam_mp = models.CharField(max_length = 255, verbose_name = 'Front camera resolution')
+
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
+
+
+class CartProduct(models.Model):
     user = models.ForeignKey('Customer', verbose_name = 'Customer', on_delete = models.CASCADE)
     cart = models.ForeignKey('Cart', verbose_name = 'Cart', on_delete = models.CASCADE, related_name =
     'related_products')
@@ -56,7 +75,6 @@ class CartProduct(models.Model):
 
 
 class Cart(models.Model):
-
     owner = models.ForeignKey('Customer', verbose_name = 'Owner', on_delete = models.CASCADE)
     products = models.ManyToManyField(CartProduct, blank = True, related_name = 'related_cart')
     total_products = models.PositiveIntegerField(default = 0)
@@ -67,7 +85,6 @@ class Cart(models.Model):
 
 
 class Customer(models.Model):
-
     user = models.ForeignKey(User, verbose_name = 'Customer', on_delete = models.CASCADE)
     phone_number = models.CharField(max_length = 20, verbose_name = 'Phone number')
     address = models.CharField(max_length = 255, verbose_name = 'Address')
