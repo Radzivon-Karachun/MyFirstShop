cb20f4109ab6e959bc3884f625d04c87940e3c2e
Radzivon-Karachun <karachun1988@gmail.com>
2021-04-11 22:54:10 +0200

    Checking the Image Resolution

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,12 +1,37 @@
 from django.contrib import admin
 from .models import *
 from .abstracts import Categories
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, ModelForm, ValidationError
+from PIL import Image
 
 # Register your models here.
 
 
+class ProductAdminForm(ModelForm):
+
+    MIN_RESOLUTION = (400, 400)
+    MAX_RESOLUTION = (2500, 2500)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = 'Upload image with min resolution of {}x{}px and max of {}x{}px'.format(
+            *self.MIN_RESOLUTION, *self.MAX_RESOLUTION)
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        max_height, max_width = self.MAX_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Image resolution less than the minimum!')
+        elif img.height > max_height or img.width > max_width:
+            raise ValidationError('Image resolution is greater than maximum!')
+        return image
+
+
 class NotebookAdmin(admin.ModelAdmin):
+
+    form = ProductAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "category":
@@ -15,6 +40,8 @@ class NotebookAdmin(admin.ModelAdmin):
 
 
 class SmartphoneAdmin(admin.ModelAdmin):
+
+    form = ProductAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "category":
