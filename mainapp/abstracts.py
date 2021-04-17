8be3901d3a5a16eeda617cc19ad3e1473cbce78f
Radzivon-Karachun <karachun1988@gmail.com>
2021-04-17 10:56:15 +0200

    Limitations on the size and resolution of uploaded images

--- mainapp/abstracts.py
+++ mainapp/abstracts.py
@@ -1,4 +1,17 @@
 from django.db import models
+from PIL import Image
+
+
+class MinResolutionErrorException(Exception):
+    pass
+
+
+class MaxResolutionErrorException(Exception):
+    pass
+
+
+class MaxSizeErrorException(Exception):
+    pass
 
 
 class Categories(models.Model):
@@ -10,6 +23,11 @@ class Categories(models.Model):
 
 
 class Product(models.Model):
+
+    MIN_RESOLUTION = (400, 400)
+    MAX_RESOLUTION = (1000, 1000)
+    MAX_IMAGE_SIZE = 3145728
+
     class Meta:
         abstract = True
 
@@ -22,3 +40,16 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        image = self.image
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        max_height, max_width = self.MAX_RESOLUTION
+        if image.size > Product.MAX_IMAGE_SIZE:
+            raise MaxSizeErrorException('Image size should not exceed 3MB!')
+        if img.height < min_height or img.width < min_width:
+            raise MinResolutionErrorException('Image resolution less than the minimum!')
+        elif img.height > max_height or img.width > max_width:
+            raise MaxResolutionErrorException('Image resolution is greater than maximum!')
+        return image
